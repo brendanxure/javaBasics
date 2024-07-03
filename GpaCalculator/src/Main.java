4dc2ecece39291cb57ca1f918f3cf76b9aaba313
brendanobilo <brendanxure@outlook.com>
2024-07-03 15:18:46 +0100

    update

--- GpaCalculator/src/Main.java
+++ GpaCalculator/src/Main.java
@@ -25,6 +25,8 @@ public class Main {
             System.out.println("You current gp is A-");
         } else if (answer >= B_MINUS_MIN) {
             System.out.println("You current gp is B");
+        } else if (answer >= C_PLUS_MIN) {
+            System.out.println("You current gp is B-");
         }
     }
 }
