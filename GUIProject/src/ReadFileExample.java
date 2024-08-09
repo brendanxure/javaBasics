f935dfed1ee4966e152aae3b59807fc7f75ee275
brendanobilo <brendanxure@outlook.com>
2024-08-09 19:23:12 +0100

    update

---
+++ GUIProject/src/ReadFileExample.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class ReadFileExample {
+    public static void main(String[] args) {
+        String fileName = "c:\\Users\\Senter\\IdeaProjects\\Files\\src\\example.txt";
+        try {
+            //System.out.println("Trying");
+            BufferedReader br = new BufferedReader(new FileReader(fileName));
+            String line;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+            br.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
